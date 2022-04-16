17c1db7f259011b1cb6f26e04df3fe0f5d8a3f26
chenxiangbao <2273657629@qq.com>
2022-04-16 22:34:22 +0800

    push commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hello.git3");
         System.out.println("dev text");
         System.out.println("master text");
+        System.out.println("push text");
 
     }
 }
