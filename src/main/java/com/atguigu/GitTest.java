4de63d3226a85f094fe0eaeff12b5270987b4f5b
chenxiangbao <2273657629@qq.com>
2022-04-16 21:40:29 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @title: GitTest
+ * @Author Tan
+ * @Date: 2022/4/16 21:24
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
